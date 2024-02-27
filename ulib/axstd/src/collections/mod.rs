075b6c90688d598bbb434679e82540358723751e
silent-rain <2367221387@qq.com>
2024-02-27 21:14:01 +0800

    更新hashmap

--- ulib/axstd/src/collections/mod.rs
+++ ulib/axstd/src/collections/mod.rs
@@ -4,10 +4,9 @@ extern crate alloc;
 #[cfg(feature = "alloc")]
 use alloc::collections::*;
 
+#[cfg(feature = "hash_map")]
 pub mod hash_map;
+#[cfg(feature = "hash_map")]
 pub use hash_map::HashMap;
 
 pub mod random;
-
-// pub use hash_map;
-// use std::collections::HashMap;
